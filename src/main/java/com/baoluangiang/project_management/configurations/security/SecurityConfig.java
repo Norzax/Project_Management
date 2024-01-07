99fa6e00475379ae26460912fa67d0098a354715
Giang Bảo Luân <giangbaoluan5@gmail.com>
2024-01-07 12:40:46 +0700

    a/setup/5/baoluan/corsConfiguration

--- src/main/java/com/baoluangiang/project_management/configurations/security/SecurityConfig.java
+++ src/main/java/com/baoluangiang/project_management/configurations/security/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -38,12 +39,15 @@ public class SecurityConfig {
             "/swagger-ui.html",
             "/api/v1/user/auth/**",
             "/api/v1/admin/auth/**",
+            "/api/v1/admin/**"
     };
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(auth -> auth
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors // Cấu hình CORS
+                        .configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())) // Cấu hình mặc định
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers(WHITE_LIST).permitAll()
                         .requestMatchers("/api/v1/admin/**").hasAuthority(RoleEnum.ADMIN.name())
                         .requestMatchers("/api/v1/user/**").hasAnyAuthority(RoleEnum.USER.name(), RoleEnum.ADMIN.name())
